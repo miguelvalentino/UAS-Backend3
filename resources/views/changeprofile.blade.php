fa7803babedba3aa35f54a2a1bcd3d33da013b74
miguelvalentino <miguelvalentiono@gmail.com>
2024-06-26 11:19:44 +0700

    update change profile

--- resources/views/changeprofile.blade.php
+++ resources/views/changeprofile.blade.php
@@ -1,20 +1,34 @@
-<x-layout>
-<x-slot:title>Change Profile</x-slot:title>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div style="border: 3px solid black;">
-    <h2>placeholder</h2>
-    <form action="/changedprofile" method="POST">
-        @csrf
-        <input name="password" type="text" placeholder="Password">
-        <input name="newEmail" type="text" placeholder="New Email">
-        <input name="newName" type="text" placeholder="New Name">
-        <button>placeholder</button>
-</body>
-</html>
-</x-layout>
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Change Profile</h2>
+        </div>
+        @vite('resources/css/app.css')
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form action="/changedprofile" method="POST">
+                @csrf
+                <div>
+                    <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                    <div class="mt-2">
+                        <input name="password" type="text" placeholder="Password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+                <div>
+                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                    <div class="mt-2">
+                        <input name="newEmail" type="text" placeholder="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+                <div>
+                    <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                    <div class="mt-2">
+                        <input name="newName" type="text" placeholder="Name" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+                <div>
+                    <p><font color="white">space</font></p>
+                    <button class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Confirm</button>
+                </div>
+            </form>
+        </div>
+    </div>
